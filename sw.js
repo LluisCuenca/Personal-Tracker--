dd61b2d6a935faea0687d0355ed11545e6ff3d7e
LluisCuenca <97266117+LluisCuenca@users.noreply.github.com>
2026-06-17 22:49:38 +0200

    up

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    LZ-String, fuentes). Nunca intercepta las llamadas de Firebase/Google para que
    la sincronización y el login sigan necesitando red. */
 
-var CACHE = 'pt-cache-v8';
+var CACHE = 'pt-cache-v9';
 var APP_SHELL = [
   './',
   './index.html',
