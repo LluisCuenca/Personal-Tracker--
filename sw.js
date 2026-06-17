e8c409843d05d5d562b9713ec9a02569caae93c1
LluisCuenca <97266117+LluisCuenca@users.noreply.github.com>
2026-06-17 20:51:32 +0200

    Auto-deploy 2026-06-17 20:51:32

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    LZ-String, fuentes). Nunca intercepta las llamadas de Firebase/Google para que
    la sincronización y el login sigan necesitando red. */
 
-var CACHE = 'pt-cache-v4';
+var CACHE = 'pt-cache-v5';
 var APP_SHELL = [
   './',
   './index.html',
